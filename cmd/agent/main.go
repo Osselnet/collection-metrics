132b5d0e14c3a416bbe19e5b571220d588d33e6e
Oleg <hornets@list.ru>
2023-06-13 12:58:21 +0300

    added configuration via environment variables

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,30 +2,49 @@ package main
 
 import (
 	"flag"
-	agent "github.com/Osselnet/metrics-collector/internal/agent"
+	"github.com/Osselnet/metrics-collector/internal/agent"
+	"github.com/caarlos0/env"
 	"log"
+	"os"
 	"time"
 )
 
 type Config struct {
-	addr           string
-	reportInterval int
-	pollInterval   int
+	Addr           string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
+	PollInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
 }
 
 func main() {
 	config := new(Config)
-	flag.StringVar(&config.addr, "a", "127.0.0.1:8080", "address to listen on")
-	flag.IntVar(&config.reportInterval, "r", 10, "write metrics to file interval")
-	flag.IntVar(&config.pollInterval, "p", 2, "write metrics to file interval")
+	flag.StringVar(&config.Addr, "a", "127.0.0.1:8080", "address to listen on")
+	flag.IntVar(&config.ReportInterval, "r", 10, "write metrics to file interval")
+	flag.IntVar(&config.PollInterval, "p", 2, "write metrics to file interval")
 	flag.Parse()
+
+	envConfig := Config{}
+	err := env.Parse(&envConfig)
+	if err != nil {
+		log.Println("Error -", err)
+	}
+
+	if _, ok := os.LookupEnv("ADDRESS"); ok {
+		config.Addr = envConfig.Addr
+	}
+	if _, ok := os.LookupEnv("REPORT_INTERVAL"); ok {
+		config.ReportInterval = envConfig.ReportInterval
+	}
+	if _, ok := os.LookupEnv("POLL_INTERVAL"); ok {
+		config.PollInterval = envConfig.PollInterval
+	}
 
 	cfg := agent.Config{
 		Timeout:        4 * time.Second,
-		PollInterval:   time.Duration(config.pollInterval) * time.Second,
-		ReportInterval: time.Duration(config.reportInterval) * time.Second,
-		Address:        config.addr,
+		PollInterval:   time.Duration(config.PollInterval) * time.Second,
+		ReportInterval: time.Duration(config.ReportInterval) * time.Second,
+		Address:        config.Addr,
 	}
+
 	agent, err := agent.New(cfg)
 	if err != nil {
 		log.Fatal(err)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/Osselnet/metrics-collector/internal/server/handlers"
 	"github.com/Osselnet/metrics-collector/internal/server/httpserver"
+	"os"
 )
 
 type Config struct {
@@ -14,6 +15,10 @@ func main() {
 	config := new(Config)
 	flag.StringVar(&config.addr, "a", "127.0.0.1:8080", "address to listen on")
 	flag.Parse()
+
+	if a := os.Getenv("ADDRESS"); a != "" {
+		config.addr = a
+	}
 
 	cfg := httpserver.Config{
 		Addr: config.addr,
