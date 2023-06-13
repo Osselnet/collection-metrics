77a0e93cec3808ce2cb10d8517f7c3b6b0525434
Oleg <hornets@list.ru>
2023-06-13 11:03:25 +0300

    bug fix

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -9,21 +9,21 @@ import (
 
 type Config struct {
 	addr           string
-	reportInterval time.Duration
-	pollInterval   time.Duration
+	reportInterval int
+	pollInterval   int
 }
 
 func main() {
 	config := new(Config)
 	flag.StringVar(&config.addr, "a", "127.0.0.1:8080", "address to listen on")
-	flag.DurationVar(&config.reportInterval, "r", 10*time.Second, "write metrics to file interval")
-	flag.DurationVar(&config.pollInterval, "p", 2*time.Second, "write metrics to file interval")
+	flag.IntVar(&config.reportInterval, "r", 10, "write metrics to file interval")
+	flag.IntVar(&config.pollInterval, "p", 2, "write metrics to file interval")
 	flag.Parse()
 
 	cfg := agent.Config{
 		Timeout:        4 * time.Second,
-		PollInterval:   config.pollInterval,
-		ReportInterval: config.reportInterval,
+		PollInterval:   time.Duration(config.pollInterval) * time.Second,
+		ReportInterval: time.Duration(config.reportInterval) * time.Second,
 		Address:        config.addr,
 	}
 	agent, err := agent.New(cfg)
