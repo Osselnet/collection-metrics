c5078245fa41966a0da849673edaab7084b5e7ef
Oleg <hornets@list.ru>
2023-07-03 22:53:40 +0300

    bug fix

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -63,7 +63,7 @@ func TestAgent_sendReport(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			h := handlers.New(chi.NewRouter(), &storage.MemStorage{
 				Metrics: metrics.New(),
-			})
+			}, "", false)
 			server := httptest.NewServer(h.GetRouter())
 			defer server.Close()
 
